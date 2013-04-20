ca6a32e17d81b3a56a38d25c0897b3cb3cdb0458
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2013-04-20 15:55:52 +0000

    updated install spatial index script

--- web/build/install_spatialindex.sh
+++ web/build/install_spatialindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # quick n dirty script to install spatialindex which is required by rtree
-wget http://download.osgeo.org/libspatialindex/spatialindex-src-1.7.1.tar.bz2
-tar xjvf spatialindex-src-1.7.1.tar.bz2
-cd spatialindex-src-1.7.1
+wget http://download.osgeo.org/libspatialindex/spatialindex-src-1.7.0.tar.bz2
+tar xjvf spatialindex-src-1.7.0.tar.bz2
+cd spatialindex-src-1.7.0
 ./configure && make && sudo make install
