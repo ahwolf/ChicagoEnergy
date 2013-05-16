8f1144b91cf590b0c3af55866197eb1c771c7e6b
Aaron Wolf <aaron.wolf@datascopeanalytics.com>
2013-05-15 23:02:40 -0500

    changed global var

--- web/apps/main/templates/main/global_var.js
+++ web/apps/main/templates/main/global_var.js
@@ -10,3 +10,30 @@ var social_media = "{{social_media}}";
 var facebook_app_id = "{{FACEBOOK_APP_ID}}";
 
 console.log("global var: ", social_media)
+
+var loadBarJSON = document.getElementById('loadBarInner');
+var sourceJSON;
+
+function loadJSON() {
+  // make loadBar skinny again
+  loadBarJSON.style.width = "1px";
+  // fade loadBar in
+  TweenLite.to($('#loadBar'), .25, {autoAlpha:1});
+
+  var queueJSON = new createjs.LoadQueue(false);
+  queueJSON.addEventListener("progress", handleJSONProgress);
+  queueJSON.addEventListener("complete", handleJSONComplete);
+  queueJSON.loadManifest([
+     { src: sourceJSON }
+  ]);
+
+  function handleJSONProgress(event) {
+    // use event.loaded to get the percentage of the loading
+    loadBarJSON.style.width = Math.ceil(event.progress * 300) + "px";
+  }
+
+  function handleJSONComplete() {
+    console.log('JSON loaded');
+    TweenLite.to($('#loadBar'), .25, {autoAlpha:0, overwrite: false});
+  }
+}
